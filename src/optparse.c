2f5984306bf8877d0789e04e4282b024b0038c3a
baimour <85350961+baimour@users.noreply.github.com>
2025-02-28 04:21:18 +0800

    Update optparse.c

--- src/optparse.c
+++ src/optparse.c
@@ -64,7 +64,7 @@ static struct option longopts[] = {
 static int usage(int e, char* prog_name)
 {
 	fprintf(stderr,
-	"Usage: %s [-"
+	"使用方法: %s [-"
 	"DEhpvVldsSTLRnPI"
 #ifdef DEV_BUILD
 			"12"
@@ -75,55 +75,55 @@ static int usage(int e, char* prog_name)
 #ifdef TUI
 			"t"
 #endif
-			"] [-e boot arguments] [-k Pongo image] [-o overlay file] [-r ramdisk file] [-K KPF file] [-i checkra1n file]\n"
+			"] [-e 引导参数] [-k Pongo图像] [-o 叠加文件] [-r ramdisk文件] [-K KPF文件] [-i checkra1n文件]\n"
 			"Copyright (C) 2024, palera1n team, All Rights Reserved.\n\n"
-			"iOS/iPadOS/tvOS 15.0-18.3, bridgeOS 5.0-9.3 arm64 jailbreaking tool\n\n"
-			"\t--version\t\t\t\tPrint version\n"
-			"\t--force-revert\t\t\t\tRemove jailbreak\n"
+			"iOS/iPadOS/tvOS 15.0-18.3, bridgeOS 5.0-9.3 arm64越狱工具\n\n"
+			"\t--version\t\t\t\t显示版本\n"
+			"\t--force-revert\t\t\t\t移除越狱\n"
 #ifdef DEV_BUILD
-			"\t-1, --test1\t\t\t\tSet palerain_option_test1\n"
-			"\t-2, --test2\t\t\t\tSet palerain_option_test2\n"
+			"\t-1, --test1\t\t\t\t设置palerain选项测试1\n"
+			"\t-2, --test2\t\t\t\t设置palerain选项测试2\n"
 #endif
 #ifdef ROOTFUL
-			"\t-B, --setup-partial-fakefs\t\tSetup partial fakefs\n"
-			"\t-c, --setup-fakefs\t\t\tSetup fakefs\n"
-			"\t-C, --clean-fakefs\t\t\tClean fakefs\n"
+			"\t-B, --setup-partial-fakefs\t\t设置部分fakefs\n"
+			"\t-c, --setup-fakefs\t\t\t设置fakefs\n"
+			"\t-C, --clean-fakefs\t\t\t清理fakefs\n"
 #endif
 			"\t-d, --demote\t\t\t\tDemote\n"
-			"\t-D, --dfuhelper\t\t\t\tExit after entering DFU\n"
-			"\t-e, --boot-args <boot arguments>\tXNU boot arguments\n"
-			"\t-E, --enter-recovery\t\t\tEnter recovery mode\n"
+			"\t-D, --dfuhelper\t\t\t\t进入DFU后退出\n"
+			"\t-e, --boot-args <引导参数>\tXNU引导参数\n"
+			"\t-E, --enter-recovery\t\t\t进入恢复模式\n"
 #ifdef ROOTFUL
-			"\t-f, --fakefs \t\t\t\tBoots fakefs\n"
+			"\t-f, --fakefs \t\t\t\t引导fakefs\n"
 #endif
-			"\t-h, --help\t\t\t\tShow this help\n"
-			"\t-i, --override-checkra1n <file>\t\tOverride checkra1n\n"
-			"\t-k, --override-pongo <file>\t\tOverride Pongo image\n"
-			"\t-K, --override-kpf <file>\t\tOverride kernel patchfinder\n"
+			"\t-h, --help\t\t\t\t显示使用帮助\n"
+			"\t-i, --override-checkra1n <file>\t\t覆盖checkra1n\n"
+			"\t-k, --override-pongo <文件>\t\t覆盖Pongo图像\n"
+			"\t-K, --override-kpf <文件>\t\t覆盖内核补丁查找器\n"
 #ifdef ROOTFUL
-			"\t-l, --rootless\t\t\t\tBoots rootless. This is the default\n"
+			"\t-l, --rootless\t\t\t\t引导Rootless，这是默认的\n"
 #endif
-			"\t-L, --jbinit-log-to-file\t\tMake jbinit log to /cores/jbinit.log (can be read from sandbox while jailbroken)\n"
-			"\t-n, --exit-recovery\t\t\tExit recovery mode\n"
-			"\t-I, --device-info\t\t\tPrint info about the connected device\n"
-			"\t-o, --override-overlay <file>\t\tOverride overlay\n"
-			"\t-p, --pongo-shell\t\t\tBoots to PongoOS shell\n"
-			"\t-P, --pongo-full\t\t\tBoots to a PongoOS shell with default images already uploaded\n"
-			"\t-r, --override-ramdisk <file>\t\tOverride ramdisk\n"
-			"\t-R, --reboot-device\t\t\tReboot connected device in normal mode\n"
-			"\t-s, --safe-mode\t\t\t\tEnter safe mode\n"
-			"\t-S, --no-colors\t\t\t\tDisable colors on the command line\n"
-			"\t-T, --telnetd\t\t\t\tEnable TELNET daemon on port 46 (insecure)\n"
-			"\t-v, --debug-logging\t\t\tEnable debug logging\n"
-			"\t\tThis option can be repeated for extra verbosity.\n"
-			"\t-V, --verbose-boot\t\t\tVerbose boot\n"
+			"\t-L, --jbinit-log-to-file\t\t将jbinit日志记录到/cores/jbinit.log (越狱后可以从沙盒中读取)\n"
+			"\t-n, --exit-recovery\t\t\t退出恢复模式\n"
+			"\t-I, --device-info\t\t\t输出已连接设备的信息\n"
+			"\t-o, --override-overlay <文件>\t\t覆盖叠加\n"
+			"\t-p, --pongo-shell\t\t\t引导至PongoOS Shell\n"
+			"\t-P, --pongo-full\t\t\t引导到已上传默认图像的PongoOS Shell\n"
+			"\t-r, --override-ramdisk <文件>\t\t覆盖ramdisk\n"
+			"\t-R, --reboot-device\t\t\t在正常模式重启已连接的设备\n"
+			"\t-s, --safe-mode\t\t\t\t进入安全模式\n"
+			"\t-S, --no-colors\t\t\t\t在命令行上禁用颜色\n"
+			"\t-T, --telnetd\t\t\t\t在端口46上启用TELNET守护程序 (insecure)\n"
+			"\t-v, --debug-logging\t\t\t启用调试日志记录\n"
+			"\t\t这个选项可以重复，以增加冗余\n"
+			"\t-V, --verbose-boot\t\t\t详细引导\n"
 
 #ifdef TUI
-			"\t-t, --tui\t\t\t\tForce interactive TUI\n"
-			"\t--cli\t\t\t\t\tForce cli mode\n"
+			"\t-t, --tui\t\t\t\t强制互动TUI\n"
+			"\t--cli\t\t\t\t\t强制cli模式\n"
 #endif
-		"\nEnvironmental variables:\n"
-		"\tTMPDIR\t\ttemporary diretory (path the built-in checkra1n will be extracted to)\n"
+		"\n环境变量:\n"
+		"\tTMPDIR\t\t临时目录 (内置checkra1n的路径将被提取到)\n"
 			,
 			prog_name);
 	exit(e);
@@ -178,7 +178,7 @@ int optparse(int argc, char* argv[]) {
 			break;
 		case 'e':
 			if (strlen(optarg) > (sizeof(xargs_cmd) - 0x20)) {
-                LOG(LOG_FATAL, "Boot arguments too long");
+                LOG(LOG_FATAL, "引导参数过长");
                 return -1;
             }
 			snprintf(xargs_cmd, sizeof(xargs_cmd), "xargs %s", optarg);
@@ -216,12 +216,12 @@ int optparse(int argc, char* argv[]) {
 			break;
 		case 'k':
 			if (access(optarg, F_OK) != 0) {
-				LOG(LOG_FATAL, "Cannot access pongo file at %s: %d (%s)", optarg, errno, strerror(errno));
+				LOG(LOG_FATAL, "无法访问%s的pongo文件: %d (%s)", optarg, errno, strerror(errno));
 				return -1;
 			}
 			pongo_path = malloc(strlen(optarg) + 1);
 			if (pongo_path == NULL) {
-				LOG(LOG_FATAL, "memory allocation failed");
+				LOG(LOG_FATAL, "内存分配失败");
 				return -1;
 			}
 			snprintf(pongo_path, strlen(optarg) + 1, "%s", optarg);
@@ -239,51 +239,51 @@ int optparse(int argc, char* argv[]) {
 				return 1;
 			struct mach_header_64* hdr = (struct mach_header_64*)override_kpf.ptr;
 			if (hdr->magic != MH_MAGIC_64 && hdr->magic != MH_CIGAM_64) {
-				LOG(LOG_FATAL, "Invalid kernel patchfinder: Not thin 64-bit Mach-O");
+				LOG(LOG_FATAL, "内核补丁查找器无效: 不薄的64位Mach-O");
 				return -1;
 			} else if (hdr->filetype != MH_KEXT_BUNDLE) {
-				LOG(LOG_FATAL, "Invalid kernel patchfinder: Not a kext bundle");
+				LOG(LOG_FATAL, "内核补丁查找器无效: 不是kext包");
 				return -1;
 			} else if (hdr->cputype != CPU_TYPE_ARM64) {
-				LOG(LOG_FATAL, "Invalid kernel patchfinder: CPU type is not arm64");
+				LOG(LOG_FATAL, "内核查找器无效: CPU类型不是arm64");
 				return -1;
 			}
 			break;
 		case 'i': {};
 			struct stat st;
 			if (stat(optarg, &st) != 0) {
-				LOG(LOG_FATAL, "cannot stat external checkra1n file: %d (%s)", errno, strerror(errno));
+				LOG(LOG_FATAL, "无法统计外部checkra1n文件: %d (%s)", errno, strerror(errno));
 				return -1;
 			} else if (!(st.st_mode & S_IXUSR) && !(st.st_mode & S_IXGRP) && !(st.st_mode & S_IXOTH)) {
-				LOG(LOG_FATAL, "%s is not executable", optarg);
+				LOG(LOG_FATAL, "%s不可执行", optarg);
 				return -1;
 			} else if (!S_ISREG(st.st_mode)) {
-				LOG(LOG_FATAL, "%s is not a regular file", optarg);
+				LOG(LOG_FATAL, "%s不是常规文件", optarg);
 				return -1;
 			}
 			if (st.st_size < (UCHAR_MAX+1)) {
-				LOG(LOG_FATAL, "%s too small", optarg);
+				LOG(LOG_FATAL, "%s太小", optarg);
 				return -1;
 			}
 			int checkra1n_fd = open(optarg, O_RDONLY);
 			if (checkra1n_fd == -1) {
-				LOG(LOG_FATAL, "Cannot open %s: %d (%s)", optarg, errno, strerror(errno));
+				LOG(LOG_FATAL, "无法打开%s: %d (%s)", optarg, errno, strerror(errno));
 				return -1;
 			}
 			void* addr = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, checkra1n_fd, 0);
 			if (addr == MAP_FAILED) {
-				LOG(LOG_ERROR, "Failed to map file %s: %d (%s)", optarg, errno, strerror(errno));
+				LOG(LOG_ERROR, "映射文件失败%s: %d (%s)", optarg, errno, strerror(errno));
 				return -1;
 			}
 			if (boyermoore_horspool_memmem(addr, st.st_size, (const unsigned char*)"[ra1npoc15-part] thanks to", strlen("[ra1npoc15-part] thanks to")) != NULL) 
 				{
 					palerain_flags |= palerain_option_checkrain_is_clone;
-					LOG(LOG_VERBOSE3, "%s is checkra1n clone", optarg);
+					LOG(LOG_VERBOSE3, "%s是checkra1n克隆", optarg);
 				}
 			else
 			{
 				palerain_flags &= ~palerain_option_checkrain_is_clone;
-				LOG(LOG_VERBOSE3, "%s is checkra1n", optarg);
+				LOG(LOG_VERBOSE3, "%s是checkra1n", optarg);
 			}
 			munmap(addr, st.st_size);
 			close(checkra1n_fd);
@@ -344,24 +344,24 @@ int optparse(int argc, char* argv[]) {
 		printf(
 			"palera1n " PALERAIN_VERSION "\n"
 			BUILD_COMMIT " " BUILD_NUMBER " (" BUILD_BRANCH ")\n\n"
-			"Build style: " BUILD_STYLE "\n"
-			"Build tag: " BUILD_TAG "\n"
+			"构建风格: " BUILD_STYLE "\n"
+			"构建标签: " BUILD_TAG "\n"
 #ifdef USE_LIBUSB
-			"USB backend: libusb\n"
+			"USB后端: libusb\n"
 #else
-			"USB backend: IOKit\n"
+			"USB后端: IOKit\n"
 #endif
-			"Build options: " BUILD_OPTIONS "\n"
+			"构建选项: " BUILD_OPTIONS "\n"
 		);
 		return 0;
 	}
 
 	if (palerain_flags & palerain_option_telnetd) {
-		LOG(LOG_WARNING, "telnetd is enabled, this is a security hole");
+		LOG(LOG_WARNING, "Telnetd已启用，这是一个安全漏洞");
 	}
 
 	if ((palerain_flags & (palerain_option_tui)) && (palerain_flags & (palerain_option_cli))) {
-		LOG(LOG_FATAL, "cannot specify both --tui and --cli");
+		LOG(LOG_FATAL, "不能同时指定--tui和--cli");
 		return -1;
 	}
 
@@ -371,7 +371,7 @@ int optparse(int argc, char* argv[]) {
 	} else {
 #ifdef ROOTFUL
 		if ((palerain_flags & (palerain_option_rootless | palerain_option_rootful)) == 0) {
-			LOG(LOG_FATAL, "Please specify rootful (-f) or rootless (-l)");
+			LOG(LOG_FATAL, "请指定rootful (-f) 或 rootless (-l)");
 			return -1;
 		}
 #else
@@ -396,7 +396,7 @@ int optparse(int argc, char* argv[]) {
 #ifdef ROOTFUL
 	if (!(palerain_flags & palerain_option_rootful)) {
 		if ((palerain_flags & palerain_option_setup_rootful)) {
-			LOG(LOG_FATAL, "Cannot setup rootful when rootless is requested. Use -f to enable rootful mode.");
+			LOG(LOG_FATAL, "指定了rootless时，无法设置rootful。使用-f启用rootful模式");
 			return -1;
 		}
 	}
@@ -410,7 +410,7 @@ int optparse(int argc, char* argv[]) {
 #ifdef NO_CHECKRAIN
 		if (checkra1n_len == 0 && ext_checkra1n == NULL)
 		{
-			LOG(LOG_FATAL, "checkra1n omitted in build but no override specified");
+			LOG(LOG_FATAL, "Checkra1n在构建中省略，但没有指定覆盖");
 			return -1;
 		}
 		if (!((palerain_flags & palerain_option_pongo_exit) || (palerain_flags & palerain_option_pongo_exit)))
@@ -418,7 +418,7 @@ int optparse(int argc, char* argv[]) {
 #ifdef NO_KPF
 			if (checkra1n_kpf_pongo_lzma_len == 0)
 			{
-				LOG(LOG_FATAL, "kernel patchfinder omitted in build but no override specified");
+				LOG(LOG_FATAL, "内核补丁程序在构建中省略，但没有指定覆盖");
 				return -1;
 			}
 #endif
@@ -427,7 +427,7 @@ int optparse(int argc, char* argv[]) {
 
 #ifdef NO_EMBED_HELPER
 	if (libcheckra1nhelper_dylib_len == 0 && gOverrideLibcheckra1nHelper == NULL) {
-			LOG(LOG_FATAL, "checkra1n helper omitted in build but no override specified");
+			LOG(LOG_FATAL, "构建中省略了checkra1n帮助器，但没有指定覆盖");
 			return -1;
 	}
 #endif
@@ -444,7 +444,7 @@ int optparse(int argc, char* argv[]) {
 		}
 		else
 		{
-			fprintf(stderr, "%s: unknown argument: %s\n", argv[0], argv[index]);
+			fprintf(stderr, "%s: 未知参数: %s\n", argv[0], argv[index]);
 			usage(1, argv[0]);
 		}
 	}
